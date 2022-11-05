21d08b0def8cc77075e9316344a870742e0c11f3
ammarkannas <ammarkannas@outlook.com>
2022-11-05 19:22:35 +0000

    update environment step

--- src/resources/views/environment-setup.blade.php
+++ src/resources/views/environment-setup.blade.php
@@ -33,6 +33,44 @@
                 </label>
                 <input type="text" class="form-control" name="app_url" id="app_url" value="{{ old('app_url') ?? config('app.url') }}">
             </div>
+
+            <div class="mb-3">
+                <div>
+                    App Debug
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input"
+                           type="radio"
+                           name="app_debug"
+                           id="app_debug_true"
+                           value="true" {{ (old('app_debug') || env('app_debug')) === true ? 'checked' : null }}>
+                    <label class="form-check-label" for="app_debug_true">True</label>
+                </div>
+
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input"
+                           type="radio"
+                           name="app_debug"
+                           id="app_debug_false"
+                           value="false" {{ (old('app_debug') || env('app_debug')) === false ? 'checked' : null }}>
+                    <label class="form-check-label" for="app_debug_false">False</label>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="log_level">
+                   Log Level
+                </label>
+
+                <select class="form-control" name="log_level" id="log_level">
+                    @foreach(['debug', 'info', 'notice', 'warning', 'error', 'critical', 'alert', 'emergency'] as $level)
+                        <option value="{{ $level }}" {{ old('log_level', env('LOG_LEVEL')) === $level ? 'selected' : null }}>
+                            {{ $level }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
         </form>
     </x-installer::card>
 @endsection
